a3dc5c2aa1c4ce890e1bde138145098bb831f66c
Connor Semb <polishmustard710@gmail.com>
2022-04-01 18:50:20 -0500

    added java code to have onClick function and are prompted with confirmLength window

--- codeStuff.js
+++ codeStuff.js
@@ -1,4 +1,28 @@
-const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const number = "0123456789";
-const symbols "!@#$%^&*+_-?><";
+// Assignment Code
+document.querySelector("#generate").addEventListener("click", writePassword);
 
+// generateBtn.addEventListener("click", writePassword);
+
+let lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+let upperCaseAlphabet = lowerCaseAlphabet.toLocaleUpperCase();
+let randomNumbers = "0123456789";
+let specialCharacters = "!@#$%^&*(){}|?><";
+// let genPassword = "";
+
+// Variable confirmations
+let confirmLength = "";
+let confirmLowerCaseAlphabet;
+let confirmUpperCaseAlphabet;
+let confirmSpecialCharacters;
+
+function generatePassword() {
+  let confirmLength = (prompt("How many characters would you like your password to be? It can be contain anywhere between 8-128 characters"));
+}
+
+alert('Your password will contain ${confirmLength} characters');
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
