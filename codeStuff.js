02aa7d49c9b2d5cdcd134560a30780a466b74640
Connor Semb <polishmustard710@gmail.com>
2022-04-01 19:22:39 -0500

    added all window prompts for each parameter

--- codeStuff.js
+++ codeStuff.js
@@ -7,22 +7,40 @@ let lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
 let upperCaseAlphabet = lowerCaseAlphabet.toLocaleUpperCase();
 let randomNumbers = "0123456789";
 let specialCharacters = "!@#$%^&*(){}|?><";
-// let genPassword = "";
+let genPassword = "";
 
 // Variable confirmations
 let confirmLength = "";
 let confirmLowerCaseAlphabet;
 let confirmUpperCaseAlphabet;
 let confirmSpecialCharacters;
+let confirmRandomNumbers;
 
+// Given a prompt to confirm password paramaters
 function generatePassword() {
   let confirmLength = (prompt("How many characters would you like your password to be? It can be contain anywhere between 8-128 characters"));
+
+  while(confirmLength <= 7 || confirmLength >= 51) {
+    alert("Password length must be between 8-50 characters Try again");
+    let confirmLength = (prompt("How many characters would you like your password to contain?"));
+    } 
+
+    alert(`Your password will have ${confirmLength} characters`);
+
+    let confirmLowerCaseAlphabet = confirm("Click OK to confirm lowercase characters");
+    let confirmUpperCaseAlphabet = confirm("Click OK to confirm uppercase characters");
+    let confirmSpecialCharacters = confirm("Click OK to confirm special characters");
+    let confirmRandomNumbers = confirm("Click OK to confirm numeric characters");
+    while(confirmLowerCaseAlphabet === false && confirmUpperCaseAlphabet === false && confirmSpecialCharacters === false && confirmRandomNumbers === false) {
+      alert("You must choose at least one parameter");
+      let confirmLowerCaseAlphabet = confirm("Click OK to confirm lowercase characters");
+      let confirmUpperCaseAlphabet = confirm("Click OK to confirm uppercase characters");
+      let confirmSpecialCharacters = confirm("Click OK to confirm special characters");
+      let confirmRandomNumbers = confirm("Click OK to confirm numeric characters");
+    }
 }
-
-alert('Your password will contain ${confirmLength} characters');
-
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
