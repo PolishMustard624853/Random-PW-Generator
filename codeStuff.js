080db55da001b2319a2fff5a7007726bb1d53fcb
Connor Semb <polishmustard710@gmail.com>
2022-04-05 15:08:02 -0500

    Added in if (confirmation) statements. Still trying to figure out how to get the password to print on screen

--- codeStuff.js
+++ codeStuff.js
@@ -4,24 +4,23 @@ document.querySelector("#generate").addEventListener("click", writePassword);
 // generateBtn.addEventListener("click", writePassword);
 
 let lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
-let upperCaseAlphabet = lowerCaseAlphabet.toLocaleUpperCase();
+let upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let randomNumbers = "0123456789";
 let specialCharacters = "!@#$%^&*(){}|?><";
 let genPassword = "";
 
-// Variable confirmations
 let confirmLength = "";
 let confirmLowerCaseAlphabet;
 let confirmUpperCaseAlphabet;
 let confirmSpecialCharacters;
 let confirmRandomNumbers;
 
-// Given a prompt to confirm password paramaters
+// Prompt to confirm password paramaters 
 function generatePassword() {
   let confirmLength = (prompt("How many characters would you like your password to be? It can be contain anywhere between 8-128 characters"));
 
-  while(confirmLength <= 7 || confirmLength >= 51) {
-    alert("Password length must be between 8-50 characters Try again");
+  while(confirmLength <= 7 || confirmLength >= 129) {
+    alert("Password length must be between 8-128 characters Try again");
     let confirmLength = (prompt("How many characters would you like your password to contain?"));
     } 
 
@@ -33,14 +32,33 @@ function generatePassword() {
     let confirmRandomNumbers = confirm("Click OK to confirm numeric characters");
     while(confirmLowerCaseAlphabet === false && confirmUpperCaseAlphabet === false && confirmSpecialCharacters === false && confirmRandomNumbers === false) {
       alert("You must choose at least one parameter");
-      let confirmLowerCaseAlphabet = confirm("Click OK to confirm lowercase characters");
-      let confirmUpperCaseAlphabet = confirm("Click OK to confirm uppercase characters");
-      let confirmSpecialCharacters = confirm("Click OK to confirm special characters");
-      let confirmRandomNumbers = confirm("Click OK to confirm numeric characters");
     }
+    if (confirmLowerCaseAlphabet) {
+      passwordCharacters = passwordCharacters(lowerCaseAlphabet);
+    }
+
+    if (confirmUpperCaseAlphabet) {
+      passwordCharacters = passwordCharacters(upperCaseAlphabet);
+    }
+
+    if (confirmSpecialCharacters) {
+      passwordCharacters = passwordCharacters(specialCharacters);
+    }
+
+    if (confirmRandomNumbers) {
+      passwordCharacters = passwordCharacters(randomNumbers);
+    }
+
+    let randomPassword = "";
+    for (var i = 0; i < confirmLength; i++) {
+      genPassword = genPassword + passwordCharacters(Math.floor(Math.random() * passwordCharacters.length));
+      console.log(genPassword)
+    }
+    return randomPassword;
 }
+
 function writePassword() {
   let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+  let passwordText = document.querySelector("#your");
   passwordText.value = password;
 }
